60aed6d9d30b25f71f07045b6169f4e2adbbbab8
Matthew Kim <mattkim83@gmail.com>
2022-03-12 16:13:15 -0800

    app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn</li>
+      </ul>
+
+      </div>
+    
   );
 }
 
 export default App;
+
+// const App = (props) => {
+//   return (
+//     <div className="App">
+//       <h1>Hello Dojo!</h1>
+//       <h2>Things I need to do:</h2>
+//       <ul>
+//         <li>Learn</li>
+//       </ul>
+
+//       </div>
+//   );
+
+// }
+// export default App;
